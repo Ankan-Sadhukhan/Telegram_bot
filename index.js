4b9b7e6e488f92857981c5c8d403ddf6915dd7e8
Ankan Sadhukhan <sadhukhanankan7@gmail.com>
2024-01-16 18:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Telegraf } = require('telegraf')
 const { message } = require('telegraf/filters')
 
-const bot = new Telegraf("6844935859:AAFMvyyyyIAVsfqw-EAF0nXHSlCGjPFIZCk")
+const bot = new Telegraf(API SECRET KEY)
 bot.start((ctx) => ctx.reply('Happy BirthDay!!'))
 bot.command('bin',(ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
