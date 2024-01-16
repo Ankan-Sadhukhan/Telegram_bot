4a97ecc0e4e9665e41da7b067589789ee97851ff
Ankan Sadhukhan <sadhukhanankan7@gmail.com>
2024-01-17 00:46:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ bot.command('bin',(ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('Thank you', (ctx) => ctx.reply('Shh!'))
 bot.launch()
+
+/**
+ * How to get the secret token for creating a bot ?
+ * -> Open Telegram and search for BotFather 
+ * -> To read instruction type /start and press enter
+ * -> to create a new bot type /newbot and presss enter
+ * -> It will ask for a bot name, give a bot name without a / , ex: coding_bot
+ * -> then it will ask for a username ending with bot, ex: code_exmaples_js_cpp_bot
+ * // t.me/code_exmaples_js_cpp_bot
+ */
